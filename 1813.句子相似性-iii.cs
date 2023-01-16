d406ac0274b498756bb5baa471c0f82eb7f81dc4
what-is-me <2454787428@qq.com>
2023-01-16 11:52:29 +0800

    2023-1-16

---
+++ 1813.句子相似性-iii.cs
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=1813 lang=csharp
+ * @lcpr version=21110
+ *
+ * [1813] 句子相似性 III
+ */
+
+// @lc code=start
+public class Solution {
+    public bool AreSentencesSimilar(string sentence1, string sentence2) {
+
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "My name is Haley"\n"My Haley"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "of"\n"A lot of words"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "Eating right now"\n"Eating"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "Luky"\n"Lucccky"\n
+// @lcpr case=end
+
+ */
+
+
